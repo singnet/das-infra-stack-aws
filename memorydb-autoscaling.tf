e17f1518b9cd6ad1880dea474292ad8cf1d45f83
levisingularity <rafael.levi@singularitynet.io>
2023-12-06 12:04:15 -0300

    feat: lambda function

--- memorydb-autoscaling.tf
+++ memorydb-autoscaling.tf
@@ -1,5 +1,5 @@
 variable "cloudwatch_alarms" {
-  type = map(object({
+  type = list(object({
     name                = string
     comparison_operator = string,
     metric_name         = string,
@@ -8,13 +8,14 @@ variable "cloudwatch_alarms" {
     statistic           = string,
     threshold           = number,
     cluster_name        = string,
-    s3_object           = string
+    s3_object           = string,
+    evaluation_periods  = number,
   }))
 }
 
 resource "aws_sns_topic" "memory_db_scaling_topic" {
-  for_each = var.cloudwatch_alarms
-  name     = "${each.value.name}Topic"
+  count = length(var.cloudwatch_alarms)
+  name  = "${var.cloudwatch_alarms[count.index].name}Topic"
 }
 
 resource "aws_cloudwatch_metric_alarm" "memory_usage_scaling_out_alarm" {
@@ -40,15 +41,49 @@ resource "aws_sns_topic_subscription" "lambda_subscription" {
   count     = length(var.cloudwatch_alarms)
   topic_arn = aws_sns_topic.memory_db_scaling_topic[count.index].arn
   protocol  = "lambda"
-  endpoint  = "ARN_LAMBDA"
+  endpoint  = aws_lambda_function.auto_scaling[count.index].arn
 }
 
-resource "aws_lambda_function" "functions" {
+resource "aws_iam_role" "iam_for_auto_scaling_lambda" {
+  name               = "memorydb_autoscaling_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "auto_scaling_policy" {
+  name        = "auto_scaling_policy"
+  description = "Policy for CloudWatch Logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "logs:CreateLogGroup",
+        Resource = "arn:aws:logs:us-east-1:038760728819:*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = aws_lambda_function.auto_scaling.*.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "logs_attachment" {
+  role       = aws_iam_role.iam_for_auto_scaling_lambda.name
+  policy_arn = aws_iam_policy.auto_scaling_policy.arn
+}
+
+resource "aws_lambda_function" "auto_scaling" {
   count         = length(var.cloudwatch_alarms)
   function_name = var.cloudwatch_alarms[count.index].name
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.iam_for_auto_scaling_lambda.arn
   runtime       = "python3.7"
-  handler       = "handler.handle"
+  handler       = "lambda_function.lambda_handler"
   s3_bucket     = "das.singularitynet.io"
   s3_key        = var.cloudwatch_alarms[count.index].s3_object
   timeout       = 180
